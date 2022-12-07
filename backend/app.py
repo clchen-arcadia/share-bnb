98b08b89e34e6deb34bb06e822da79db7e3ea439
clchen-arcadia <clchen.arcadia@gmail.com>
2022-12-06 19:18:06 -0500

    Get signup view function working, accepts body json and returns json

--- backend/app.py
+++ backend/app.py
@@ -3,13 +3,33 @@ import os
 from dotenv import load_dotenv
 from flask_debugtoolbar import DebugToolbarExtension
 
-from flask import Flask, render_template, request, flash, redirect, session, g
+from sqlalchemy.exc import (IntegrityError)
 
-from models import db, connect_db
+from forms import (
+    UserAddForm, UserEditForm, LoginForm, MessageForm, UserSignup
+)
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    flash,
+    redirect,
+    session,
+    g,
+    jsonify
+)
+
+from werkzeug.datastructures import MultiDict
+
+from models import db, connect_db, User
 
 app = Flask(__name__)
 
 load_dotenv()
+
+CURR_USER_KEY = "curr_user"
+
 
 
 
@@ -23,6 +43,83 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 toolbar = DebugToolbarExtension(app)
 
+app.config['WTF_CSRF_ENABLED'] = False
+
 connect_db(app)
 
-#####################
+##############################################################################
+# app.before_requests---these run before every request!
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+# @app.before_request
+# def add_csrf_to_g():
+#     """Add a csrf form to g"""
+
+#     g.csrf_form = CSRFProtectForm()
+
+##############################################################################
+# Helper functions
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Log out user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+##############################################################################
+# Routes
+
+@app.route('/signup', methods=["POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+    data = MultiDict(mapping=request.json)
+    form = UserSignup(data)
+
+    # ..., meta={'csrf': False}
+
+
+    if form.validate():
+
+        username = data["username"]
+        password = data["password"]
+        email = data["email"]
+        first_name = data["first_name"]
+        last_name = data["last_name"]
+
+        try:
+            user = User.signup(username, email, password, first_name, last_name)
+            db.session.commit()
+
+        except IntegrityError:
+            return jsonify(errors=IntegrityError)
+
+
+        print("TEST>>>>>>>>>>>>>>>>> user.to_dict", user.to_dict)
+        return jsonify(user.to_dict())
+
+    else:
+        return jsonify(errors=form.errors)
---
+++ backend/forms.py
@@ -0,0 +1,108 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, InputRequired, Optional, URL
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    """ Form for users to edit their profiles. """
+
+    username = StringField(
+        "Username",
+        validators=[
+            Length(max=20),
+            InputRequired()
+        ]
+    )
+
+    email = StringField(
+        "Email",
+        validators=[
+            Length(max=50),
+            InputRequired(),
+            Email()
+        ]
+    )
+
+    image_url = StringField(
+        "User Profile Picture",
+        validators=[
+            Optional(),
+            URL()
+        ]
+    )
+
+    header_image_url = StringField(
+        "Profile Banner Image",
+        validators=[
+            Optional(),
+            URL()
+        ]
+    )
+
+    bio = StringField(
+        "User Bio",
+        validators=[
+            Optional()
+        ]
+    )
+
+    password = PasswordField(
+        "password",
+        validators=[
+            InputRequired()
+        ]
+    )
+
+class CSRFProtectForm(FlaskForm):
+    """For protection against cross-site request forgery"""
+
+#####################################################################
+
+class UserSignup(FlaskForm):
+    """Form for signing up a new user"""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired()]
+    )
+
+    password = StringField(
+        "Password",
+        validators=[InputRequired()]
+    )
+
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email()]
+    )
+
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired()]
+    )
+
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired()]
+    )
