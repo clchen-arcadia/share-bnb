66b9e1f9d7d615336f90f07efcb25464842cfef8
Ashley Mathew <amathew195@gmail.com>
2022-12-09 13:46:21 -0600

    added listing card skeleton

--- frontend/share-bnb/src/ListingCard.js
+++ frontend/share-bnb/src/ListingCard.js
@@ -1,13 +1,27 @@
-
+import { useNavigate } from 'react-router-dom';
 
 // TODO: get bootstraps on the cards here
-function ListingCard() {
+function ListingCard({ listing }) {
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate(`/companies/${listing.id}`);
+  }
 
   return (
-    <div className="ListingCard">
-      ListingCard
+    <div onClick={handleClick} className="CompanyCard card">
+      <div className="card-body">
+        <h6>{listing.name}</h6>
+        <p><small>{listing.description}</small></p>
+        {listing.logoUrl &&
+          <img
+            src={listing.logoUrl}
+            alt="logo"
+            className="float-end ms-5 position-absolute top-0 end-0 p-2"></img>}
+      </div>
     </div>
-  )
+  );
 }
 
 export default ListingCard;
