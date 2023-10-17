8bd41da42f8fb4abd9a61947ad83919aa835e736
clchen-arcadia <clchen.arcadia@gmail.com>
2023-10-17 10:09:21 -0400

    Add functionality for the Listing Card
    Access the photo src. Put image to the top of the card

--- frontend/src/ListingCard.js
+++ frontend/src/ListingCard.js
@@ -11,12 +11,12 @@ function ListingCard({ listing }) {
   return (
     <div onClick={handleClick} className="ListingCard">
       <div className="card-body">
+        <img
+          src={listing.photo}
+          alt={listing.title}
+        />
         <h6>{listing.title}</h6>
         <p><small>{listing.description}</small></p>
-        <img
-            src="test.com"
-            alt={listing.title}
-            className="float-end ms-5 position-absolute top-0 end-0 p-2"></img>
       </div>
     </div>
   );
