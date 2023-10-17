e48d4b7063af3f1698043f1b6d18a58d08b59dcd
clchen-arcadia <clchen.arcadia@gmail.com>
2023-10-17 13:08:48 -0400

    Add ternary to display user name or hint for new user

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,11 +1,27 @@
+import { useContext } from 'react';
+import userContext from './userContext';
 
 function Homepage() {
+  const context = useContext(userContext);
+  const firstName = context?.data?.firstName;
+  const isLoggedIn = firstName !== undefined;
 
   return (
     <div className="Homepage">
-      Homepage
+      <h1>
+        Welcome to ShareBnB!
+      </h1>
+      {
+        isLoggedIn
+          ? <p>Welcome back {firstName}!</p>
+          : <p>
+            Hint: Sign up with a username/password/email (you can use 'email@email.com') or sign in with:
+            <br />username: u1
+            <br />password: password
+          </p>
+      }
     </div>
-  )
+  );
 }
 
 export default Homepage;
