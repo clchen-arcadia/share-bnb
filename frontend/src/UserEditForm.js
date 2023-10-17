d5f7bf0d0ec10a46bce16c91ca706a48aac5922c
clchen-arcadia <clchen.arcadia@gmail.com>
2023-10-17 13:08:24 -0400

    Display user info on page
    Future todo: add edit form

--- frontend/src/UserEditForm.js
+++ frontend/src/UserEditForm.js
@@ -1,10 +1,23 @@
-
+import { useContext } from 'react';
+import userContext from './userContext';
 
 function UserEditForm() {
+  const context = useContext(userContext);
+  const userInfo = context.data
 
   return (
     <div className="UserEditForm">
-      UserEditForm
+      <h1>
+        User Information
+      </h1>
+      <p>
+        First Name: {userInfo.firstName}
+        <br />Last Name: {userInfo.lastName}
+        <br />Email: {userInfo.email}
+        <br />Is Admin: {String(userInfo.isAdmin)}
+        <br />Is A Host: {String(userInfo.isHost)}
+      </p>
+
     </div>
   )
 }
