714ca5cb0ed3a85405df72afc9fdb89374fb0167
clchen-arcadia <clchen.arcadia@gmail.com>
2023-10-17 12:17:23 -0400

    Add users listings to page

--- frontend/src/UsersListingsPage.js
+++ frontend/src/UsersListingsPage.js
@@ -1,12 +1,67 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ListingCard from './ListingCard';
+import ShareBnbApi from './Api';
+import axios from 'axios';
 
 
 function UsersListingsPage() {
+  const { curr_user } = useParams();
+
+  const [pageData, setPageData] = useState({
+    data: null,
+    isLoading: true,
+  });
+  console.log("UsersListingsPage rendered with pageData]=", pageData);
+
+  useEffect(
+    function loadListingsOnMount() {
+      // console.debug("ListingPage useEffect load", "pageData]=", pageData);
+
+      async function getUserListings() {
+        const listings = await ShareBnbApi.getUserListings(curr_user);
+
+        const photoPromises = [];
+        for (let i = 0; i < listings.length; i++) {
+          const listing = listings[i];
+          photoPromises.push(ShareBnbApi.getFirstPhoto(listing.id));
+        }
+
+        await axios.all(photoPromises).then(axios.spread(function (...photos) {
+          for (let i = 0; i < listings.length; i++) {
+            const listing = listings[i];
+            listing.photo = photos[i];
+          }
+        }));
+
+        setPageData({
+          data: listings,
+          isLoading: false,
+        });
+      }
+
+      getUserListings();
+    },
+    []
+  );
+
 
   return (
     <div className="UsersListingsPage">
       <div>
         UsersListingsPage
       </div>
+      {
+        pageData.isLoading
+          ? <p>Loading!</p>
+          : pageData.data.map(
+            (l) => {
+              return <ListingCard
+                key={l.id}
+                listing={l}
+              />;
+            })
+      }
       <button>
         Add a Listing
       </button>
