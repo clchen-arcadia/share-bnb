fbcb2bf24b3d40bd93b751a28d23d66804672d24
clchen-arcadia <clchen.arcadia@gmail.com>
2023-10-18 18:26:28 -0400

    Reformat with no try catch, includes breakpoint

--- backend/token_helpers.py
+++ backend/token_helpers.py
@@ -6,20 +6,22 @@ my_secret_key = os.getenv('SECRET_KEY')
 
 
 def create_token(user):
+    breakpoint()
+
     username = user.username
     is_admin = user.is_admin
     is_host = user.is_host
 
-    try:
+    payload = {
+        'username': username,
+        'is_admin': is_admin,
+        'is_host': is_host
+    }
 
-        payload = {
-            'username': username,
-            'is_admin': is_admin,
-            'is_host': is_host
-        }
-        token = jwt.encode(payload,
-                           my_secret_key, algorithm='HS256')
-        return token
+    token = jwt.encode(
+        payload,
+        my_secret_key,
+        algorithm='HS256'
+    )
 
-    except Exception as e:
-        raise e
+    return token
